38327eb068575b5ac1b62dfaca58b7477628d924
Zclipse7 <bernaldezaaron@yahoo.com>
2024-06-03 18:33:51 +0800

    ui fixes

---
+++ app/src/main/java/com/example/mobilepose/ProductManagement.java
@@ -0,0 +1,77 @@
+package com.example.mobilepose;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductManagement#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductManagement extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProductManagement() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProductManagement.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProductManagement newInstance(String param1, String param2) {
+        ProductManagement fragment = new ProductManagement();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_management, container, false);
+    }
+
+    public void CreateProduct(View view){
+        //code to ADD Product to database
+    }
+    public void UpdateProduct(View view){
+        //code to UPDATE Product to database
+    }
+    public void SearchProduct(View view){
+        //code to SEARCH Product to database
+    }
+    public void DeleteProduct(View view){
+        //code to DELETE Product to database
+    }
+}
