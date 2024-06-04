efc5448858d967e4caec65ebc431c427cb8527b2
Zclipse7 <bernaldezaaron@yahoo.com>
2024-06-04 22:42:10 +0800

    ui fixes

--- app/src/main/java/com/example/mobilepose/ProductManagement.java
+++ app/src/main/java/com/example/mobilepose/ProductManagement.java
@@ -2,11 +2,20 @@ package com.example.mobilepose;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +32,12 @@ public class ProductManagement extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    String[] productCategory={"Add New Category"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> arrayAdapter;
+    Button cancelButton,saveButton;
+    EditText newCatEdit;
 
     public ProductManagement() {
         // Required empty public constructor
@@ -58,8 +73,28 @@ public class ProductManagement extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_management, container, false);
+        View view= inflater.inflate(R.layout.fragment_product_management, container, false);
+
+        autoCompleteTextView=view.findViewById(R.id.autoCompleteTextView3);
+        arrayAdapter=new ArrayAdapter<String>(getActivity(),R.layout.list_item,productCategory);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String item=adapterView.getItemAtPosition(position).toString();
+
+                if (item.equals("Add New Category")){
+                    ShowAddCategory();
+                }else{
+
+                }
+
+            }
+        });
+
+        return view;
     }
 
     public void CreateProduct(View view){
@@ -73,5 +108,44 @@ public class ProductManagement extends Fragment {
     }
     public void DeleteProduct(View view){
         //code to DELETE Product to database
+    }
+
+    public void AddNewCategory(){
+        //code to add category to database
+    }
+
+    public void ShowAddCategory(){
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                requireContext(), R.style.BottomSheetDialogTheme
+        );
+        View bottomSheetView = LayoutInflater.from(requireContext())
+                .inflate(
+                        R.layout.add_category_pop,
+                        (ConstraintLayout) getActivity().findViewById(R.id.addCategory)
+                );
+
+        newCatEdit=bottomSheetView.findViewById(R.id.newCatEdit);
+        cancelButton=bottomSheetView.findViewById(R.id.cancelCatBtn);
+        saveButton=bottomSheetView.findViewById(R.id.saveCatBtn);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewCategory();
+            }
+        });
+
+
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+
+
     }
 }
