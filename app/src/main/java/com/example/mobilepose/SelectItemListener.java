d9752db2e8506b7c3bf3d1566a109a468e5257aa
Zclipse7 <bernaldezaaron@yahoo.com>
2024-06-12 02:33:31 +0800

    ui fixes

---
+++ app/src/main/java/com/example/mobilepose/SelectItemListener.java
@@ -0,0 +1,10 @@
+package com.example.mobilepose;
+
+import com.example.mobilepose.ChildItem;
+import com.example.mobilepose.Model.User;
+
+public interface SelectItemListener {
+
+    void onItemClick(ChildItem childitem);
+
+}
