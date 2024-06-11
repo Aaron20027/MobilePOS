e28e42a85776370f96a24329949c93176601157e
Zclipse7 <bernaldezaaron@yahoo.com>
2024-06-12 02:32:47 +0800

    ui fixes

--- app/src/main/java/com/example/mobilepose/ChildItem.java
+++ app/src/main/java/com/example/mobilepose/ChildItem.java
@@ -2,28 +2,30 @@ package com.example.mobilepose;
 
 public class ChildItem {
 
-    // Declaration of the variable
     private String ChildItemTitle;
     private String ChildItemPrice;
 
-    // Constructor of the class
-    // to initialize the variable*
     public ChildItem(String childItemTitle, String childItemPrice)
     {
         this.ChildItemTitle = childItemTitle;
         this. ChildItemPrice= childItemPrice;
     }
 
-    // Getter and Setter method
-    // for the parameter
     public String getChildItemTitle()
     {
         return ChildItemTitle;
     }
 
-    public void setChildItemTitle(
-            String childItemTitle)
+    public void setChildItemTitle(String childItemTitle)
     {
         ChildItemTitle = childItemTitle;
+    }
+
+    public String getChildItemPrice() {
+        return ChildItemPrice;
+    }
+
+    public void setChildItemPrice(String childItemPrice) {
+        ChildItemPrice = childItemPrice;
     }
 }
--- app/src/main/java/com/example/mobilepose/ParentItem.java
+++ app/src/main/java/com/example/mobilepose/ParentItem.java
@@ -5,21 +5,14 @@ import com.example.mobilepose.ChildItem;
 import java.util.List;
 
 public class ParentItem {
-
-    // Declaration of the variables
     private String ParentItemTitle;
     private List<ChildItem> ChildItemList;
 
-    // Constructor of the class
-    // to initialize the variables
     public ParentItem(String ParentItemTitle, List<ChildItem> ChildItemList)
     {
         this.ParentItemTitle = ParentItemTitle;
         this.ChildItemList = ChildItemList;
     }
-
-    // Getter and Setter methods
-    // for each parameter
     public String getParentItemTitle()
     {
         return ParentItemTitle;
